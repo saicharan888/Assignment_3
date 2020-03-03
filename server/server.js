79c4eaa4b98fb1c9cc2709e24488a1a7a5a75f15
Sai charan katta <saicharan.katta888@gmail.com>
2020-03-02 20:11:32 -0800

    Update server.js
    second commit

--- server/server.js
+++ server/server.js
@@ -7,15 +7,7 @@ const { ApolloServer } = require('apollo-server-express');
 
 let aboutMessage = "Product Inventory";
 const productDB = [
-  {
-      id:1,category: 'Shirts', name: 'Blue Shirt', Price: 60.00,Image:'https://www.superdry.com/us/mens/jackets/details/153408/skate-luxe-coach-jacket--navy'
-  },
-  {
-      id: 2,category: 'Accessories', name: 'Scarf ', Price: 20.00, Image:'https://www.gap.com/browse/product.do?pid=546615022#pdp-page-content'
-  },
-  {
-      id: 3,category: 'Jackets', name: 'Leather jacket', Price: 210.00,     Image:'https://www.everlane.com/products/womens-leather-flight-jacket-mellowpink?locale=US&utm_medium&utm_source=pla-google&utm_campaign=838503582&utm_content=408119694860&utm_term=aud-492596540375:pla-837884946428&adgroup=90496873874&pid=6803-48009&device=c&gclid=Cj0KCQiAtOjyBRC0ARIsAIpJyGORgeKDYeVjQzHW6LL2d9_30MzWd_ayNK4e33WDeGYfNB850kWF7SIaAv3tEALw_wcB'
-  }
+  
 ];
 
 
